6dcb4ebc945ee4e99397fa1beafba2951790c214
gagikharutyunyan12 <gagikharutyunyan189@gmail.com>
2023-09-27 01:33:09 -0700

    add visual tool, add sidebar test, fix tests

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -26,7 +26,6 @@ export default {
         '<rootDir>src',
     ],
     testMatch: [
-        // Обнаружил разницу между МАК ОС и ВИНДОУС!!!
         '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
     ],
     rootDir: '../../',
