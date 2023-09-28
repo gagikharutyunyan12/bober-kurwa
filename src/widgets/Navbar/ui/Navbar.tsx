e0a891ebe70f258ff26951ad8f63bca38fb05906
gagikharutyunyan12 <gagikharutyunyan189@gmail.com>
2023-09-28 04:38:16 -0700

    add modal, portal, change header

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
+import { Modal } from 'shared/ui/Modal/Modal';
+import React, { useCallback, useState } from 'react';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -9,12 +11,20 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
+    const [isAuthModal, setAuthModal] = useState<boolean>(false);
 
+    const onToggleModal = useCallback(() => {
+        setAuthModal((prev) => !prev);
+    }, []);
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                /
-            </div>
+            <Button theme={ThemeButton.CLEAR_INVERTED} className={cls.links} onClick={onToggleModal}>
+                {t('Войти')}
+            </Button>
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                {/* eslint-disable-next-line */}
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci aliquid blanditiis corporis distinctio ducimus eligendi exercitationem labore mollitia nihil obcaecati odit pariatur possimus reprehenderit, totam velit vero vitae voluptatem.
+            </Modal>
         </div>
     );
 };
