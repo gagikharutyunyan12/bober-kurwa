13aa9904f1bb2c60ba501c55fdf6751b19585f69
gagikharutyunyan12 <gagikharutyunyan189@gmail.com>
2023-09-29 03:53:51 -0700

    add json-server

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -21,7 +21,6 @@ export const Modal = ({ className, children, isOpen, onClose }: ModalProps) => {
     const mods: Record<string, boolean> = {
         [cls.opened]: isOpen,
         [cls.isClosing]: isClosing,
-        [cls[theme]]: true,
     };
 
     const closeHandler = useCallback(() => {
